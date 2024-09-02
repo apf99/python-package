1b3560dbc7c2853b73b10b15bbb8be25c5458118
Andrew Friedman <andrew@cyberdreaming.net>
2024-09-01 21:16:47 -0700

    Initial commot

---
+++ main.py
@@ -0,0 +1,8 @@
+import invoice
+
+invoice.generate('invoices', 'output', 'pythonhow.png', 'product_id',
+                 'product_name', 'amount_purchased', 'price_per_unit',
+                 'total_price')
+
+
+
